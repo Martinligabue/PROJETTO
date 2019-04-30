b9cdeec74d5240baeba2ba728958a8a6f5e9933e
Martin Ligabue <martinligabue@gmail.com>
2019-04-30 12:36:27 +0200

    Update Cspostadispari.asm

--- algortimi funzionanti/Cspostadispari.asm
+++ algortimi funzionanti/Cspostadispari.asm
@@ -6,14 +6,14 @@
 	li $v0,4
 	la $a0,testoorig
 	syscall
-	
+
 	li $v0,4
 	la $a0,acapo #a capo
 	syscall
-	
+
 	la $t0,testoorig
 
-ciclo:#	stampa una a finché non 0 
+ciclo:#	stampa una a finche' non 0 
 	lb $t3,($t0)
 	beq $t3,0,exit
 	add $t2,$t3,4
@@ -21,13 +21,13 @@ ciclo:#	stampa una a finché non 0
 	mfhi $t2
 	sb $t2,($t0) #imposta la A nella posizione di memoria del primo byte
 	add $t0,$t0,2#incrementa il contatore
-	
+
 	j ciclo
-	
+
 exit:
 	li $v0,4
 	la $a0,testoorig
 	syscall
-	
+
 	li $v0,10
 	syscall
