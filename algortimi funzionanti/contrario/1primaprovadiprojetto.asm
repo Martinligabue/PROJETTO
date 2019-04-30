b28d1f1cd80fbaa9051e7eb915715c43f55b7e0d
Martin Ligabue <martinligabue@gmail.com>
2019-04-30 13:27:02 +0200

    aggiunta linea per evitare l'underflow

--- algortimi funzionanti/contrario/1primaprovadiprojetto.asm
+++ algortimi funzionanti/contrario/1primaprovadiprojetto.asm
@@ -6,27 +6,28 @@
 	li $v0,4
 	la $a0,testoorig
 	syscall
-	
+
 	li $v0,4
 	la $a0,acapo #a capo
 	syscall
-	
+
 	la $t0,testoorig
-ciclo:#	stampa una a finché non 0 
+ciclo:#ciclo
 	lb $t3,($t0)
 	beq $t3,0,exit
-	subi $t2,$t3,4
+	subi $t2,$t3,4#toglie 4 posizioni all'ascii
+	addi $t2,$t2,256#aggiunge 256 per evitare l'underflow in caso di file danneggiato
 	div $t2,$t4
 	mfhi $t2
 	sb $t2,($t0) #imposta la A nella posizione di memoria del primo byte
 	add $t0,$t0,1#incrementa il contatore
-	
+
 	j ciclo
-	
+
 exit:
 	li $v0,4
 	la $a0,testoorig
 	syscall
-	
+
 	li $v0,10
 	syscall
