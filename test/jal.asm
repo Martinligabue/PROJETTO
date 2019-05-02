296aa1a2a1e79b47fdfcc403efd9ae590bb3738e
Martin Ligabue <martinligabue@gmail.com>
2019-05-02 09:25:36 +0200

    aggiorno

--- test/jal.asm
+++ test/jal.asm
@@ -1,1 +1,25 @@
-#test epr provare jal e j $ra
+.text
+main:
+    li $t1,1
+    li $t2,3
+    jal funzione
+    move $v0,$a0
+    jal stampa
+    li $t1,2
+    li $t2,5
+    jal funzione
+    move $v0,$a0
+    jal stampa
+    j exit
+
+funzione:
+    addi $v0,$t1,$t2
+    j $ra
+
+stampa:
+    li $v0,1
+    syscall
+
+exit:
+    li $v0,10
+    syscall
