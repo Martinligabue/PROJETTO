5cea38962ce572b9ad02e452d14fb0aabb10e881
gianmarco99 <50139738+gianmarco99@users.noreply.github.com>
2019-05-02 11:37:47 +0200

    ho messo apposto il programma di martin

--- test/jal.asm
+++ test/jal.asm
@@ -1,25 +1,25 @@
 .text
 main:
-    li $t1,1
-    li $t2,3
+    li $a0,1
+    li $a1,3
     jal funzione
-    move $v0,$a0
+    move $a0,$v0
     jal stampa
-    li $t1,2
-    li $t2,5
+    li $a0,2
+    li $a1,5
     jal funzione
-    move $v0,$a0
+    move $a0,$v0
     jal stampa
     j exit
 
 funzione:
-    addi $v0,$t1,$t2
-    j $ra
+    add $v0,$a0,$a1
+    jr $ra
 
 stampa:
     li $v0,1
     syscall
-
+	jr $ra
 exit:
     li $v0,10
     syscall
