2d2f24a09b73dc41ef33ead9083eba4a02364f3d
Martin Ligabue <martinligabue@gmail.com>
2019-05-02 13:32:28 +0200

    salva una lettera in una posizione giusta

--- algortimi funzionanti/invertiE.asm
+++ algortimi funzionanti/invertiE.asm
@@ -13,36 +13,43 @@ fraseiniziale:
 
 contalunghezzatesto:
     lb $t1,($t0)
-    beqz $t1,main
+    beqz $t1,reset
     addi $t0,$t0,1
+    addi $t2,$t2,1
     j contalunghezzatesto
 reset:
-	move $t0,$s3#salvo lunghezza testo
+	move $s3,$t2#salvo lunghezza testo
 	li $t1,0
+    li $t2,0
 	move $t0,$s0
-main:
+    j main
+main:#questo ciclo fa lettera per lettera
 	beq $s2,$s3,esci
 	lb $t1,($t0)
-	jal trovaposizione
-	
-	addi $s2,$s2,1 #contatore numerico
-	addi $t0,$t0,1 #contatore indirizzico
+	j trovaposizione
+
+	jal spostatesto
 	j main
-	
+
+spostatesto:
+    addi $s2,$s2,1 #contatore numerico
+    addi $t0,$t0,1 #contatore indirizzico
+    jr $ra
+
 tornaalmain:
-	addi $s2,$s2,1 #contatore numerico
-	addi $t0,$t0,1 #contatore indirizzico
+	jal spostatesto
 	j main
-	
+
 trovaposizione:
 	move $t3,$t0
-	addi $t3,$t3,1
+    jal	spostatesto
+    addi $t3,$t3,1
 	lb $t4,($t3)
-	beq $t4,'-',trovatotrattino 
+	beq $t4,'-',trovatotrattino
 	beq $t4,' ',tornaalmain #si ferma
 	#altrimenti c'e' un numero
 	j trovaposizione
-	
+
 trovatotrattino:
 	addi $t3,$t3,1
 	lb $t4,($t3)
@@ -54,19 +61,21 @@ trovatotrattino:
 nontrattino:
 	bne $t5,' ',nonspazio
 	j doppiaalmeno
-	
+
 nonspazio:
 doppiaalmeno:
 salvacifra:
 	subi $t4,$t4,48
-	la $t0,buffer
+	la $t6,buffer
 	j carica
-	
+
 carica:
 	addi $t5,$t5,1
-	addi $t0,$t0,1 #spostiamo l'indiririzzo...
-	blt $t5,$t4,carica #...tante volte quante ne servono a un contatore per arrivare a t4
-	sb $t1,($t0) #salviamo in quella posizione la lettera
+	addi $t6,$t6,1 #spostiamo l'indiririzzo...
+	ble $t5,$t4,carica #...tante volte quante ne servono a un contatore per arrivare a t4
+	sb $t1,($t6) #salviamo in quella posizione la lettera
+	jal spostatesto
+	j trovaposizione
 esci:
 	li $v0,10
 	syscall
