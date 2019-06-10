ca68d5db757befa772e6152520d993590df19cf2
Martin Ligabue <martinligabue@gmail.com>
2019-06-10 17:25:18 +0200

    b,bjjj

--- algortimi funzionanti/invertiE.asm
+++ algortimi funzionanti/invertiE.asm
@@ -5,14 +5,12 @@ end: .asciiz "la frase cifrata era"
 sbuffer: .space 256
 
 .text
-	
+
 	la $s0, testo
 	li $s1,-1			#facciamo in modo che non sia zero senno' il beqz risulta subito uguale a zero
-	
 	j lunghezzatesto
 
-	
-lunghezzatesto:				#ciclo che...
+lunghezzatesto:				#ciclo grande, da cmabiare di nome
 
 	#beqz $s1,uscitaseria
 	lb $s1,($s0)			#salviamo in s1 la letteraaaaa
@@ -20,16 +18,16 @@ lunghezzatesto:				#ciclo che...
 	lb $t0,($s0)
 	bne $t0,'-',uscitaseria
 	addi $s0,$s0,1
-	
+
 	j leggesalvanumero
-	
+
 leggesalvanumero:
 
 	lb $s2,($s0)			#salviamo in t2 il primo numero
 	subi $s2,$s2,48
-	
+
 	j controllonumero
-	
+
 controllonumero:
 
 	addi $s0,$s0,1
@@ -40,19 +38,17 @@ controllonumero:
 	move $t2,$t0
 	mul $s2,$s2,10
 	add $s2,$s2,$t2
-	
+
 	j controllonumero
-	
-	
+
 converteprim:
 
 	la $t1, sbuffer 			#salviamo in t1 il buffer per poterci salvare la roba dentro
 	add $t1,$t1,$s2
 	sb $s1,($t1)
 	addi $s0,$s0,1
-	
+
 	j leggesalvanumero
-	
 
 convertesec:
 
@@ -63,33 +59,16 @@ convertesec:
 	li $s1,-1
 
 	j lunghezzatesto
-	
-	
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
 
 uscitaseria:
- 	
+
  	li $v0, 4
  	la $a0, end
  	syscall
- 	
+
  	li $v0, 4
 	la $a0, sbuffer
 	syscall
-	
+
 	li $v0,10
 	syscall
