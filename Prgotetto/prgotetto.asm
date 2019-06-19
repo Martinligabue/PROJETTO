125d6a387073b373d996336fd9210acc40ea0128
riccardoZ99 <50078599+riccardoZ99@users.noreply.github.com>
2019-06-19 16:14:55 +0200

    probabile costrutto algoritmi inversi
    ho corretto la chiusura dello stack, prima erano store ward mentre doveva essere load word e ho provato ad implementare l'algoritmo inverso di A ma evidentemente andava in overflow "nell'execute apparivano solo puntini"

--- Prgotetto/prgotetto.asm
+++ Prgotetto/prgotetto.asm
@@ -96,13 +96,14 @@ lunghezzabuffer2:
 
 dopo:
 
-	move $s1, $t1
+	move $s1, $t1					#salvo in $s1 e $s7 la lunghezza del testo CHIAVE
+	move $s7, $t1					
 	la $t0, buffer2
 
 sceltaalgoritmo:
 
 	lb $t2, ($t0)
-	ble $s1, 0, uscita			#bisogna mettere algoritmi inverso
+	ble $s1, 0, preinverti			#bisogna mettere algoritmi inverso
 	beq $t2, 'A', algoritmoA
 	beq $t2, 'B', algoritmoB
 	beq $t2, 'C', algoritmoC
@@ -114,11 +115,30 @@ sceltaalgoritmo:
 	syscall
 
 	j uscita
+	
+preinverti:
+
+	la $t0, buffer2				#salvo in $t0 il testo chiave e lo faccio partire dall'ultimo carattere
+	addi $t0, $t0, 4
+	
+invertialgoritmi:
+
+	lb $t2, ($t0)
+	ble $s7, 0, fine			#da impostare il metodo fine per far terminare il programma
+	beq $t2, 'A', invertialgoritmoA
+	beq $t2, 'B', invertialgoritmoB
+	beq $t2, 'C', invertialgoritmoC
+	beq $t2, 'D', invertialgoritmoD
+	beq $t2, 'E', invertialgoritmoE
+	
+	subi $t0, $t0, 1
+	
+	j invertialgoritmi
 
 algoritmoA:
 
 	move $t7, $t0
-	la $t0,buffer			 	#possiamo sovrascrivere t0
+	la $t0, buffer			 	#possiamo sovrascrivere t0
 
 cicloA:						#	stampa il carattere aumentato di 4
 
@@ -203,73 +223,84 @@ salvaStack:
 	sw $t0,0($sp)
 	sw $s0,4($sp)
 	sw $s1,8($sp)
-	move $s3,$s0 #imposta a $s3 la lunghezza di buffer
+	move $s3,$s0 				#imposta a $s3 la lunghezza di buffer
 resetValori:
-li $t0,0
-li $t9,0
-li $s1,0
+
+	li $t0,0
+	li $t9,0
+	li $s1,0
+	
 inizio:
-	la $s0,buffer		#impostiamo le variabili $s0, e $s2 rispettivamente a testo e spazio
-	la $s2,bufferTemp		#da impostare con un buffer temporaneo
-	move $t0, $s0		#creiamo una variable con lo stesso valore di -testo-
+
+	la $s0,buffer				#impostiamo le variabili $s0, e $s2 rispettivamente a testo e spazio
+	la $s2,bufferTemp			#da impostare con un buffer temporaneo
+	move $t0, $s0				#creiamo una variable con lo stesso valore di -testo-
 	li $s1, -1
 
 spaziocarattere:
+
 	li $t8, ' '
 	sb $t8, ($s2)
 	addi $s2,$s2,1
 	j lettura
 
 prelettura:
-	li $t1,0 		#si resettano $t0 e $t1, cosi' che il ciclo di conta non interferisca
+
+	li $t1,0 				#si resettano $t0 e $t1, cosi' che il ciclo di conta non interferisca
 	move $t0, $s0
 
 lettura:
+
 	bge $s1,$s3,rirpristinaStack
-	lb $t1,($t0) 		#carico in $t1 la prima lettera del testo(f)
-	move $s4,$t0 		#in $s4 mettiamo l'indirizzo della lettera che analizzeremo per poi utilizzarlo in "controllodx"
-	addi $t0,$t0,1 		#contatore dell'indirizzo della lettera
-	addi $s1,$s1,1 		#contatore della lettera che leggiamo
-	li $t2, 0	 	#contatore che parte da 0 per il controllo a sx####forse, se abbiamo voglia, lo mettiamo nel resetta
-	move $t3, $s0 		#si resetta sempre al primo indirizzo del testo
+	lb $t1,($t0) 				#carico in $t1 la prima lettera del testo(f)
+	move $s4,$t0 				#in $s4 mettiamo l'indirizzo della lettera che analizzeremo per poi utilizzarlo in "controllodx"
+	addi $t0,$t0,1 				#contatore dell'indirizzo della lettera
+	addi $s1,$s1,1 				#contatore della lettera che leggiamo
+	li $t2, 0	 			#contatore che parte da 0 per il controllo a sx####forse, se abbiamo voglia, lo mettiamo nel resetta
+	move $t3, $s0 				#si resetta sempre al primo indirizzo del testo
 
 	j controllosx
 
 controllosx:
-	bge $t2,$s1, salvalettera     #finche $t2(contatore posizione) e' minore o uguale di $s1(contatore lettera esaminata) continua ciclo
-	addi $t2,$t2,1 		#contatore per il controllo a sx
-	lb $t4, ($t3)		#carico in $t4 il carattere da controllare
-	addi $t3,$t3,1 		#contatore per la posizione del carattere da controllare
-	beq $t1,$t4, lettura	#controllo se i due caratteri sono uguali
+
+	bge $t2,$s1, salvalettera     		#finche $t2(contatore posizione) e' minore o uguale di $s1(contatore lettera esaminata) continua ciclo
+	addi $t2,$t2,1 				#contatore per il controllo a sx
+	lb $t4, ($t3)				#carico in $t4 il carattere da controllare
+	addi $t3,$t3,1 				#contatore per la posizione del carattere da controllare
+	beq $t1,$t4, lettura			#controllo se i due caratteri sono uguali
 
 	j controllosx
 
 salvalettera:
-	li $t3,0 		#$t3 diventa una variabile numerica uguale a 0 (non e' piu' un indirizzo)
-	sb $t1, ($s2) 		#si carica la lettera non doppia nello space
-	addi $s2,$s2,1		#si aumenta lo space per passare alla prossima posizione
-	move $t5,$s1 		#$t5 diventa il contatore che verra' utilizzato in controllo a dx per non modificare $s1 e poterlo riutilizzare
+
+	li $t3,0 				#$t3 diventa una variabile numerica uguale a 0 (non e' piu' un indirizzo)
+	sb $t1, ($s2) 				#si carica la lettera non doppia nello space
+	addi $s2,$s2,1				#si aumenta lo space per passare alla prossima posizione
+	move $t5,$s1 				#$t5 diventa il contatore che verra' utilizzato in controllo a dx per non modificare $s1 e poterlo riutilizzare
 
 controllodx:
+
 	bge $t5,$s3, spaziocarattere
 
-	lb $t4,($s4) 		#in $t4 carichiamo la lettera da confrontare
-	move $s6,$t5		#$s6 diventa il valore numerico di $t5 e lo utilizzeremo in "ciclonumero"
+	lb $t4,($s4) 				#in $t4 carichiamo la lettera da confrontare
+	move $s6,$t5				#$s6 diventa il valore numerico di $t5 e lo utilizzeremo in "ciclonumero"
 
-	addi $s4,$s4,1		#si sposta di 1 l'indirizzo della lettera a destra da confrontare
-	addi $t5,$t5,1		#contatore per controllo a destra
-	beq $t1,$t4, scriviposizione #se lettera non doppia e' uguale a quella che scorre a destra salva la posizione
+	addi $s4,$s4,1				#si sposta di 1 l'indirizzo della lettera a destra da confrontare
+	addi $t5,$t5,1				#contatore per controllo a destra
+	beq $t1,$t4, scriviposizione	 	#se lettera non doppia e' uguale a quella che scorre a destra salva la posizione
 
 	j controllodx
 
 scriviposizione:
+
 	li $t8, '-'
 	sb $t8, ($s2)
 	addi $s2,$s2,1
 	j ciclonumero
 
 ciclonumero:
-	subi $sp,$sp, 4 	#apre uno stack
+
+	subi $sp,$sp, 4 			#apre uno stack
 	addi $t9,$t9,1
 	li $s5,10
 	div $s6, $s5
@@ -280,31 +311,64 @@ ciclonumero:
 	j ciclonumero
 
 caricaNumero:
+
 	subi $t9,$t9,1
 	lw $t3,0($sp)
 	addi $sp,$sp,4
 	addi $t3,$t3,48
-	sb $t3,($s2) 		#inserisce il valore preso dallo stack come carattere in space
+	sb $t3,($s2) 				#inserisce il valore preso dallo stack come carattere in space
 	addi $s2,$s2,1
-	bge $t9,1,caricaNumero	#quando fa piu' di un ciclo fa il ciclo
+	bge $t9,1,caricaNumero			#quando fa piu' di un ciclo fa il ciclo
 	j controllodx
 
 rirpristinaStack:
-sw $t0,0($sp)
-sw $s0,4($sp)
-sw $s1,8($sp)
-addi $sp,$sp,12
+	
+	lw $t0, 0($sp)
+	lw $s0, 4($sp)
+	lw $s1, 8($sp)
+	addi $sp, $sp, 12
 
-j sceltaalgoritmo
+	j exit
+	
+	
+invertialgoritmoA:
 
+	move $t7, $t0
+	la $t0, buffer	
+	
+cicloinvA:
+
+	lb $t3, ($t0)
+	beq $t3, 0, exitinvertito
+	subi $t2, $t3, 4			#toglie 4 posizioni all'ascii
+	addi $t2, $t2, 256			#aggiunge 256 per evitare l'underflow in caso di file danneggiato
+	div $t2, $t4
+	mfhi $t2
+	sb $t2, ($t0)				#imposta la lettera nella posizione di memoria del primo byte
+	add $t0, $t0, 1				#incrementa il contatore
+
+	j cicloinvA
+	
+invertialgoritmoB:
+	#inverti B
+	j exitinvertito
+
+invertialgoritmoC:
+	#inverti C
+	j exitinvertito
+
+invertialgoritmoD:
+	#inverti D
+	j exitinvertito
+
+invertialgoritmoE:
+	#inverti e
+	j exitinvertito
 
 uscita:
 
 	li $v0, 10
 	syscall
-
-
-
 
 exit:
 
@@ -313,10 +377,22 @@ exit:
 	subi $s1, $s1, 1
 	j sceltaalgoritmo
 
+exitinvertito:
 
-
+	move $t0, $t7
+	addi $t0, $t0, -1
+	subi $s7, $s7, 1
+	
+	j invertialgoritmi
+	
 errore:
 
 	li $v0, 4
 	la $a0, testo
 	syscall
+	
+fine:
+	
+	li $v0, 10
+	syscall
+	
