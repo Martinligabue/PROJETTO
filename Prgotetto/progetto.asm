152a51b385925f66554a715cea73b773024b4cac
Martin Ligabue <martinligabue@gmail.com>
2019-09-02 11:36:39 +0200

    steak

--- Prgotetto/progetto.asm
+++ Prgotetto/progetto.asm
@@ -621,14 +621,32 @@ stampadecriptato:
 ############################################################
 salvaStack: #228
 
-	addi $sp,$sp,-12
+	addi $sp,$sp,-40
 	sw $t0,0($sp)
-	sw $s0,4($sp)
-	sw $s1,8($sp)
+	sw $t1,4($sp)
+	sw $t2,8($sp)
+	sw $t3,12($sp)
+	sw $t4,16($sp)
+	sw $t5,20($sp)
+	sw $t6,24($sp)
+	sw $t7,28($sp)
+	sw $t8,32($sp)
+	#sw $ra,36($sp)
+	jr $ra
 
-ripristinaStack: ####??????
-	lw...
-	addi $sp,$sp,12
+ripristinaStack: #attenzione al nome
+	lw $t0,0($sp)
+	lw $t1,4($sp)
+	lw $t2,8($sp)
+	lw $t3,12($sp)
+	lw $t4,16($sp)
+	lw $t5,20($sp)
+	lw $t6,24($sp)
+	lw $t7,28($sp)
+	lw $t8,32($sp)
+	#lw $ra,36($sp)
+	addi $sp,$sp,40
+	jr $ra
 
 exit:
 
